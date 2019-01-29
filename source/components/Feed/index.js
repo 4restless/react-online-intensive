1faa07705c9f0494702e1ab7b0d179979af513ff
4restless <4restless@gmail.com>
2019-01-29 18:48:28 +0200

    ++

--- source/components/Feed/index.js
+++ source/components/Feed/index.js
@@ -10,12 +10,30 @@ import Post from 'components/Post';
 import Styles from './styles.m.css';
 
 export default class Feed extends Component {
+    state = {
+        posts: [
+            { id: '1', comment: 'Hi all!', created: 1521521521521 },
+            { id: '2', comment: 'I am here!', created: 1521521521522 },
+        ],
+    };
+
     render() {
+        const { posts } = this.state;
+
+        const postsJSX = posts.map((post) => {
+            return (
+                <Post
+                    key = { post.id }
+                    { ...post }
+                />
+            );
+        });
+
         return (
             <section className = { Styles.feed }>
                 <StatusBar />
                 <Composer />
-                <Post />
+                { postsJSX }
             </section>
         );
     }
